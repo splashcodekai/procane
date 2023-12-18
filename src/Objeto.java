3f960978f30f7139e5402bb038a2e06a05c098e1
black cat <kaikoliverlima@gmail.com>
2023-12-18 12:25:24 -0300

    caneta azul

---
+++ src/Objeto.java
@@ -0,0 +1,14 @@
+public class Objeto {
+
+    public static void main(String []args){
+        Produto caneta= new Produto();
+        System.out.println(caneta.getCor());
+        System.out.println(caneta.getProdutoCaneta());
+
+    }
+    /**
+     * @return comentario aqui. metodo main ali fechado
+     */
+
+
+}
---
+++ src/Produto.java
@@ -0,0 +1,20 @@
+public class Produto {
+    private int produtoCaneta= 1;
+    private String cor= ("azul");
+
+    public int getProdutoCaneta() {
+        return produtoCaneta;
+    }
+
+    public void setProdutoCaneta(int produtoCaneta) {
+        this.produtoCaneta = produtoCaneta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
